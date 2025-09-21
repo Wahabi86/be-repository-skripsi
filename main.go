fc472860859a91d8f96ed61dbb74e6803acdc6de
Wahabi86 <gustiabi97@gmail.com>
2025-09-21 22:26:23 +0800

    setup cors for collaborations with frontend

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"pratikum-repository-skripsi/config"
 	"pratikum-repository-skripsi/routes"
 
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,11 +18,21 @@ func main() {
 	// Inisialisasi router Gin
 	router := gin.Default()
 
+	// Konfigurasi CORS custom
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // hanya izinkan frontend React (URL FE)
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	// Setup Routes (panggil folder routes)
 	routes.SetupRoutes(router)
 
-	// Running
-  if err := router.Run(":8080"); err != nil {
-        log.Fatal("Server gagal dijalankan:", err)
-    }
+	// Running server
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
 }
